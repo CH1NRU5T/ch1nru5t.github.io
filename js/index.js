500c23b72783ee1bdb3fd11ea67439c9c24bac81
Ansh <chinrust@gmail.com>
2021-03-13 23:45:22 +0530

    .

--- js/index.js
+++ js/index.js
@@ -3,9 +3,12 @@ function sendEmail() {
     Host: "smtp.gmail.com",
     Username: "abhifloyer@gmail.com",
     Password: "nahimilegatujhe",
-    To: "abhifloyer@gmail.com",
+    To: "chinrust@gmail.com",
     From: "abhifloyer@gmail.com",
     Subject: "hello",
     Body: "Hello",
   }).then((message) => alert("mail sent successfully"));
 }
+
+
+795da72e-5161-4641-b82f-7a35a442096d
