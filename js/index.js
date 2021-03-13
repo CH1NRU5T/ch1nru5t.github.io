9895303f9d396f5aa8ff41b06dd9e9960d89a11c
Ansh <chinrust@gmail.com>
2021-03-14 01:37:50 +0530

    .

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 function sendEmail() {
   Email.send({
-    Host: "smtp.gmail.com",
-    Username: "abhifloyer@gmail.com",
-    Password: "nahimilegatujhe",
-    To: "chinrust@gmail.com",
-    From: "abhifloyer@gmail.com",
-    Subject: "hello",
-    Body: "Hello",
-  }).then((message) => alert("mail sent successfully"));
+    SecureToken: "13b95cff-a579-4f75-bec8-e30a58fcaeec",
+    To: "abhifloyer@gmail.com",
+    From: document.getElementById("s_email").value,
+    Subject: document.getElementById("s_subject").value,
+    Body: document.getElementById("s_body").value,
+  }).then((message) => alert(message));
 }
 
-
-795da72e-5161-4641-b82f-7a35a442096d
+// 795da72e - 5161 - 4641 - b82f - 7a35a442096d
+// 2a4c53b5 - 41df - 4bcf - 9e05 - 656d03432bd8
+// fa2e515c - 6c84 - 4c1c - b886 - 1bef7d190c66
+// 13b95cff-a579-4f75-bec8-e30a58fcaeec
